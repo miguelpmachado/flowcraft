6561cb52eec97712306983da0633c080c661dcf3
Tiago Jesu <tiagofilipe12@gmail.com>
2018-10-09 15:44:51 +0100

    stringified depth for file name

--- flowcraft/templates/downsample_fastq.py
+++ flowcraft/templates/downsample_fastq.py
@@ -133,7 +133,8 @@ def main(sample_id, fastq_pair, genome_size, depth, clear, seed):
         # print ("Writing R1.fq.gz")
         ps = subprocess.Popen(('seqtk', 'sample', parsed_seed, p1, str(ratio)),
                               stdout=subprocess.PIPE)
-        with open('{}_ss_{}.fq.gz'.format(bn1, target_depth), 'w') as outfile:
+        with open('{}_ss_{}.fq.gz'.format(bn1, str(target_depth)), 'w') as \
+                outfile:
             subprocess.Popen(('gzip', '--fast', '-c'),
                              stdin=ps.stdout, stdout=outfile )
         ps.wait()
@@ -141,7 +142,8 @@ def main(sample_id, fastq_pair, genome_size, depth, clear, seed):
         # print ("Writing R2.fq.gz")
         ps = subprocess.Popen(('seqtk', 'sample', parsed_seed, p2, str(ratio)),
                               stdout=subprocess.PIPE)
-        with open('{}_ss_{}.fq.gz'.format(bn2, target_depth), 'w') as outfile:
+        with open('{}_ss_{}.fq.gz'.format(bn2, str(target_depth)), 'w') as \
+                outfile:
             subprocess.Popen(('gzip', '--fast', '-c'),
                              stdin=ps.stdout, stdout=outfile)
         ps.wait()
@@ -156,8 +158,8 @@ def main(sample_id, fastq_pair, genome_size, depth, clear, seed):
                     os.remove(rp)
 
     else:
-        os.symlink(p1, "{}_ss_{}.fq.gz".format(bn1, target_depth))
-        os.symlink(p2, "{}_ss_{}.fq.gz".format(bn2, target_depth))
+        os.symlink(p1, "{}_ss_{}.fq.gz".format(bn1, str(target_depth)))
+        os.symlink(p2, "{}_ss_{}.fq.gz".format(bn2, str(target_depth)))
 
     # Record the original estimated coverage
     with open(".report.json", "w") as fh:
